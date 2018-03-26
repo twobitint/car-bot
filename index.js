c9575c634dc696c3b8507f81e5408a43e1b1bf63
Justin Labak <jtlabak@gmail.com>
2018-03-25 19:32:31 -0700

    make all the car things

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
 require('dotenv').config();
-
 const Discord = require("discord.js");
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const request = require('request');
+const jsonRequest = require('request-json');
 const og = require('open-graph');
+
 const client = new Discord.Client();
 const token = process.env.DISCORD_API_TOKEN;
 const id = process.env.DISCORD_BOT_ID;
@@ -29,40 +30,67 @@ client.login(token);
 
 function checkUrl(url, msg) {
   if (!url.startsWith('http')) {
-    removeMsg(msg);
     return;
   }
 
-  var xhttp = new XMLHttpRequest();
-  xhttp.open('HEAD', url);
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == this.DONE) {
-      var type = this.getResponseHeader('Content-type');
-      if (type == null) {
-        removeMsg(msg);
-      } else if (type.startsWith('text/html')) {
+  var isDiscord = url.startsWith('https://cdn.discordapp');
+
+  request.get(url).on('response', function (resp) {
+    resp.setEncoding('base64');
+    //imageString = "data:" + resp.headers["content-type"] + ";base64,";
+    imageString = '';
+    resp.on('data', (data) => { imageString += data});
+    resp.on('end', () => {
+      var type = resp.headers['content-type'];
+      var image = null;
+      if (isDiscord) {
+        image = {
+          content: imageString
+        };
+      } else {
+        image = {
+          'source': { 'imageUri': url }
+        };
+      }
+      if (type.startsWith('text/html')) {
         og(url, function(err, meta){
-          if ((meta.image && meta.image.type && meta.image.type == 'image/gif')
-            || (meta.video) || (meta.type && meta.type == 'video.other')) {
-            //
-          } else {
-            removeMsg(msg);
+          if (meta.image && meta.image.type && meta.image.type != 'image/gif') {
+            checkIfCar(image, msg);
           }
         });
-      } else if (!type.startsWith('image/gif') 
-        && !type.startsWith('video/webm')
-          && !type.startsWith('video/mp4')) {
-        removeMsg(msg);
+      } else if (type.startsWith('image') && type != 'image/gif') {
+        checkIfCar(image, msg);
       }
-    }
-  };
-  xhttp.onerror = function () {
-    removeMsg(msg);
-  };
-  xhttp.send();
+    });
+  }).on('error', (e) => {
+      console.log(`Got error: ${e.message}`);
+  });
 }
 
-function removeMsg(msg) {
-  msg.delete();
-  msg.reply('Only gifs in here!');
+function checkIfCar(image, msg) {
+  var googleURL = 'https://vision.googleapis.com/v1/images:annotate?key=' + process.env.GOOGLE_VISION_API_KEY;
+  var client = jsonRequest.createClient(googleURL);
+  var payload = {
+    'requests': [
+      {
+        'image': image,
+        'features': [
+          {
+            'type': 'LABEL_DETECTION'
+          }
+        ]
+      }
+    ]
+  };
+
+  client.post(googleURL, payload, function (err, res, body) {
+    console.log(body.responses[0].labelAnnotations);
+    if (body.responses && body.responses[0].labelAnnotations) { 
+      body.responses[0].labelAnnotations.forEach(function (label) {
+        if (label.description == 'car') {
+          msg.reply('Hey I think there\'s a car in that picture');
+        }
+      });
+    }
+  });
 }
